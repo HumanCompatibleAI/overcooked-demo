bb3cf94e20be5ed2fd5561325cd97c49018ba26d
Rohin Shah <rohinmshah@gmail.com>
2019-06-15 00:21:06 -0700

    Features work, frontend is still horrible

--- static/js/demo/index.js
+++ static/js/demo/index.js
@@ -27,93 +27,112 @@ let PARAMS = {
 
 
 let layouts = {
-    "Cramped Room":[
+    "cramped_room":[
         "XXPXX",
         "O  2O",
         "X1  X",
         "XDXSX"
     ],
-    "Asymmetric Advantages":[
+    "asymmetric_advantages":[
         "XXXXXXXXX",
         "O XSXOX S",
         "X   P 1 X",
         "X2  P   X",
         "XXXDXDXXX"
     ],
-    "Coordination Ring":[
+    "coordination_ring":[
         "XXXPX",
         "X 1 P",
         "D2X X",
         "O   X",
         "XOSXX"
     ],
-    "Counter Circuit":[
-        "XXXPPXXX",
-        "X      X",
-        "D XXXX S",
-        "X2    1X",
-        "XXXOOXXX"
-    ],
-    "Forced Coordination": [
+    "random0":[
         "XXXPX",
         "O X1P",
         "O2X X",
         "D X X",
         "XXXSX"
+    ],
+    "random3": [
+        "XXXPPXXX",
+        "X      X",
+        "D XXXX S",
+        "X2    1X",
+        "XXXOOXXX"
     ]
 };
 
-let agent_layout_names = {
-    "Cramped Room": "cramped_room",
-    "Asymmetric Advantages": "asymmetric_advantages",
-    "Coordination Ring": "coordination_ring",
-    "Counter Circuit": "random0",
-    "Forced Coordination": "random3"
+function getRadioValue(id) {
+    return $("input[name=" + id + "]:checked").val();
 };
 
-$(document).ready(() => {
+let game;
+
+function startGame(endOfGameCallback) {
     let AGENT_INDEX = 1 - PARAMS.PLAYER_INDEX;
-    var setup_exp_pages = function () {
+    /***********************************
+          Set up websockets server
+    ***********************************/
+    // let HOST = "https://lit-mesa-15330.herokuapp.com/".replace(/^http/, "ws");
+    // let gameserverio = new GameServerIO({HOST});
 
-        /***********************************
-               Set up websockets server
-         ***********************************/
-        // let HOST = "https://lit-mesa-15330.herokuapp.com/".replace(/^http/, "ws");
-        // let gameserverio = new GameServerIO({HOST});
+    let model_type = getRadioValue("player2");
+    let layout_name = getRadioValue("layout");
+    let layout = layouts[layout_name];
 
-        let layout_name = 'Cramped Room';
-	let agent_layout_name = agent_layout_names[layout_name];
-	let layout = layouts[layout_name];
-
-	getOvercookedPolicy(PARAMS.MODEL_TYPE, agent_layout_name, AGENT_INDEX).then(function(npc_policy) {
-	    let npc_policies = {};
-	    npc_policies[AGENT_INDEX] = npc_policy;
-            let game = new OvercookedSinglePlayerTask({
-                container_id: "overcooked",
-		player_index: PARAMS.PLAYER_INDEX,
-                start_grid : layout,
-		npc_policies: npc_policies,
-                TIMESTEP : PARAMS.TIMESTEP_LENGTH,
-                MAX_TIME : PARAMS.MAIN_TRIAL_TIME, //seconds
-                init_orders: ['onion'],
-                always_serve: 'onion',
-                completion_callback: () => {
-		    console.log("Time up");
-                },
-                timestep_callback: (data) => {
-                    data.layout_name = layout_name;
-                    data.layout = layouts[layout_name];
-                },
-                DELIVERY_REWARD: PARAMS.DELIVERY_POINTS
-            });
-            $("#overcooked").css("text-align", "center");
-            game.init();
+    getOvercookedPolicy(model_type, layout_name, AGENT_INDEX).then(function(npc_policy) {
+	let npc_policies = {};
+	npc_policies[AGENT_INDEX] = npc_policy;
+	$("#overcooked").empty();
+        game = new OvercookedSinglePlayerTask({
+	    container_id: "overcooked",
+	    player_index: PARAMS.PLAYER_INDEX,
+	    start_grid : layout,
+	    npc_policies: npc_policies,
+	    TIMESTEP : PARAMS.TIMESTEP_LENGTH,
+	    MAX_TIME : PARAMS.MAIN_TRIAL_TIME, //seconds
+	    init_orders: ['onion'],
+	    always_serve: 'onion',
+	    completion_callback: () => {
+		console.log("Time up");
+		endOfGameCallback();
+	    },
+	    DELIVERY_REWARD: PARAMS.DELIVERY_POINTS
         });
+        game.init();
+    });
+}
 
-    };
+function endGame() {
+    game.close();
+}
 
-    /*******************
-     * Run Task
-     ******************/
-    setup_exp_pages();
+// Handler to be added to $(document) on keydown when a game is not in
+// progress, that will then start the game when Enter is pressed.
+function startGameOnEnter(e) {
+    // Do nothing for keys other than Enter
+    if (e.which !== 13) {
+	return;
+    }
+
+    disableEnter();
+    // Reenable enter handler when the game ends
+    startGame(enableEnter);
+}
+
+function enableEnter() {
+    $(document).keydown(startGameOnEnter);
+    $("#control").html("<p>Press enter to begin!</p>");
+}
+
+function disableEnter() {
+    $(document).off("keydown");
+    $("#control").html('<button id="reset">Reset</button>');
+    $("#reset").click(endGame);
+}
+
+$(document).ready(() => {
+    $("#overcooked").css("text-align", "center");
+    enableEnter();
 });
