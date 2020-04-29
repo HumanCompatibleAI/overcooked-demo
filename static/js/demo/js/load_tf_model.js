a453efb9cec1ae23511253151b3a67c75acaff27
micahcarroll <micah.d.carroll@gmail.com>
2020-04-29 08:24:27 -0700

    Fixing the replay function

--- static/js/demo/js/load_tf_model.js
+++ static/js/demo/js/load_tf_model.js
@@ -38,7 +38,9 @@ export default function getOvercookedPolicy(model_type, layout_name, playerIndex
     return modelPromise.then(function (model) {
 		return new Promise(function(resolve, reject) {
 		    resolve(function (state, game) {
-			let action_tensor = model.execute(preprocessState(state, game, playerIndex));
+			let [result, shape] = game.mdp.featurize_state(state, playerIndex);
+			let state_tensor = tf.tensor(result, shape);
+			let action_tensor = model.execute(state_tensor);
 			let action_probs = action_tensor.arraySync()[0];
 			let action_index; 
 			if (argmax == true) {
@@ -54,90 +56,6 @@ export default function getOvercookedPolicy(model_type, layout_name, playerIndex
 	    });
 	});
     });
-}
-
-const BASE_FEATURE_INDICES = {
-    "P": 10,
-    "X": 11,
-    "O": 12,
-    "D": 13,
-    "S": 14
-};
-
-// The soup numbers are duplicated in preprocessState
-const VARIABLE_FEATURE_INDICES = {
-    "onions_in_pot": 15,
-    "onions_cook_time": 16,
-    "onion_soup_loc": 17,
-    "dish": 18,
-    "onion": 19
-};
-
-function preprocessState(state, game, playerIndex) {
-    // All of our models have a batch size of 30, but we only want to predict on
-    // a single state, so we put zeros everywhere else.
-    let terrain = game.mdp.terrain_mtx;
-    let shape = [30, terrain[0].length, terrain.length, 20];
-    let result = constant(0, shape);
-
-    function handle_object(obj) {
-	let [x, y] = obj.position;
-	if (obj.name === 'soup') {
-	    let [soup_type, num_onions, cook_time] = obj.state;
-	    if (terrain[y][x] === 'P') {
-    		result[0][x][y][15] = num_onions;
-    		result[0][x][y][16] = Math.min(cook_time, 20);
-	    } else {
-    		result[0][x][y][17] = 1;
-	    }
-	} else {
-	    let feature_index = VARIABLE_FEATURE_INDICES[obj.name];
-    	    result[0][x][y][feature_index] = 1;
-	}
-    }
-
-    for (let i = 0; i < state.players.length; i++) {
-	let player = state.players[i];
-	let [x, y] = player.position;
-	let orientation = Direction.DIRECTION_TO_INDEX[player.orientation];
-	if (playerIndex === i) {
-	    result[0][x][y][0] = 1;
-	    result[0][x][y][2 + orientation] = 1;
-	} else {
-	    result[0][x][y][1] = 1;
-	    result[0][x][y][6 + orientation] = 1;
-	}
-
-	if (player.has_object()) {
-	    handle_object(player.held_object);
-	}
-    }
-
-    let pos_dict = game.mdp._get_terrain_type_pos_dict();
-    for (let ttype in BASE_FEATURE_INDICES) {
-	let t_index = BASE_FEATURE_INDICES[ttype];
-        for (let i in pos_dict[ttype]) {
-            let [x, y] = pos_dict[ttype][i];
-	    result[0][x][y][t_index] = 1;
-        }
-    }
-
-    for (let i in state.objects) {
-	let obj = state.objects[i];
-	handle_object(obj);
-    }
-    return tf.tensor(result, shape);
-}
-
-function constant(element, shape) {
-    function helper(i) {
-	let size = shape[i];
-	if (i === shape.length - 1) {
-	    return Array(size).fill(element);
-	}
-	return Array(size).fill().map(() => helper(i+1));
-    }
-    return helper(0);
 }
 
 function argmax(array) {
