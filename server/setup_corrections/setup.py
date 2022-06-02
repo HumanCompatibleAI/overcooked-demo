addab68e0b22cb3d1522e9879b76e4fb4e3597ce
alexlichtenstein <alexander@lichtenstein.com>
2022-06-02 18:10:22 +0200

    M1 deployment progress

---
+++ server/setup_corrections/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='human_aware_rl',
+      version='0.0.1',
+      description='This package has shared components.',
+      author='Micah Carroll',
+      author_email='micah.d.carroll@berkeley.edu',
+      packages=find_packages(),
+      install_requires=[
+        'GitPython',
+        'memory_profiler',
+        'sacred',
+        'pymongo',
+        'dill',
+        'matplotlib',
+        'requests',
+        'numpy==1.19.5',
+        'seaborn==0.9.0',
+        'pygame==2.1.0',
+      ],
+    )
